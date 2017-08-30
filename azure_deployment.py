d2a6e58a9d0eb8bd74c79de40b6e4cbe8051f929
Dan C Williams <dan.c.williams@gmail.com>
2017-08-30 16:48:08 -0500

    more testing test

--- azure_deployment.py
+++ azure_deployment.py
@@ -1,4 +1,5 @@
-import os.path
+import os
+import os.patha
 from deployer import Deployer
 
 
@@ -10,9 +11,7 @@ from deployer import Deployer
 
 my_subscription_id = os.environ.get('AZURE_SUBSCRIPTION_ID', '11111111-1111-1111-1111-111111111111')   # your Azure Subscription Id
 my_resource_group = 'cumulustest'            # the resource group for deployment
-my_pub_ssh_key_path = os.path.expanduser('/home/travis/build/dancwilliams/azure_dynamic_test_env/id_rsa.pub')   # the path to your rsa public key file
-
-print (my_pub_ssh_key_path)
+my_pub_ssh_key_path = os.getcwd()  # the path to your rsa public key file
 
 msg = "\nInitializing the Deployer class\n"
 print(msg)
